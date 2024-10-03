f35004aab268f2059839bf3605274d225b5ba0c8
Matiwowwski <matiwitkowski333@gmail.com>
2024-10-03 20:39:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -312,7 +312,7 @@ const sendNotification = async (employee, formattedDate, reportNumber) => {
 };
 
 // Zaplanuj zadanie na każdą minutę, ale blokuj wysyłanie, jeśli nie jest 15:20
-cron.schedule('* * * * *', async () => {
+cron.schedule('* 17 * * *', async () => {
     const today = new Date();
     const formattedDate = today.toLocaleDateString('pl-PL'); // Użyj formatu polskiego
 
@@ -398,7 +398,7 @@ const sendReminder = async (employee, formattedDate, reportNumber) => {
 };
 
 // Zaplanuj przypomnienie na 5 dni po dacie zadania, ale wysyłaj tylko o 15:20
-cron.schedule('* * * * *', async () => {
+cron.schedule('* 17 * * *', async () => {
     const today = new Date();
     const reminderDate = new Date();
     reminderDate.setDate(today.getDate() - 5); // Ustaw datę na 5 dni przed dzisiejszą
