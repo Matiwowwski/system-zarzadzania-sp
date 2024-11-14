c56102a0d99dc7778056d810f191820aa6d98c7e
Matiwowwski <matiwitkowski333@gmail.com>
2024-11-14 17:37:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ app.delete('/workdays/:id', async (req, res) => {
 });
 
 // URL webhooka
-const webhookUrl = 'https://discord.com/api/webhooks/1299764781041062019/AduCrdUGtqYLfrqJiiYqltNW4TcoWqcSwaQ78iX0zU5jfo64qva6KUn2FCiJc8LWpE_E';
+const webhookUrl = 'https://discord.com/api/webhooks/1223000074804789248/HOAzQ0FX6xhNglD_wv4N5JCB86IFqODQ0N5G2Z-4dIW0Bm7Vul8IHi7zQtZE0XpKEhNE';
 
 // Mapa użytkowników
 const userMap = {
@@ -236,7 +236,7 @@ const isCorrectTime = () => {
     console.log(`Aktualny czas w Warszawie: ${hours}:${minutes} ${period}`);
 
     // Sprawdzenie, czy jest 12:56 PM
-    return hours === 12 && minutes === 00 && period === 'PM';
+    return hours === 5 && minutes === 40 && period === 'PM';
 };
 
 // Zaplanuj zadanie na każdą minutę od północy do 1 w nocy
@@ -261,7 +261,7 @@ const sendNotification = async (employees, formattedDate, reportNumbers) => {
     try {
         // Oblicz datę powiadomienia na 23:00 dnia poprzedniego
         const notificationDate = new Date();
-        notificationDate.setDate(notificationDate.getDate() + 4);
+        notificationDate.setDate(notificationDate.getDate() + 5);
         notificationDate.setHours(23, 0, 0, 0);
 
         const timestamp = Math.floor(notificationDate.getTime() / 1000);
