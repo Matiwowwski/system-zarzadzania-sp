95431b58b5b28c2618e2fd0cd1463c5e92c52d6b
Matiwowwski <matiwitkowski333@gmail.com>
2024-11-14 17:27:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -236,7 +236,7 @@ const isCorrectTime = () => {
     console.log(`Aktualny czas w Warszawie: ${hours}:${minutes} ${period}`);
 
     // Sprawdzenie, czy jest 12:56 PM
-    return hours === 5 && minutes === 26 && period === 'PM';
+    return hours === 12 && minutes === 00 && period === 'PM';
 };
 
 // Zaplanuj zadanie na każdą minutę od północy do 1 w nocy
